6463f6341b1cb38ec3a83b0be63e230cbaa940b8
LewoDev <leventozkan89@gmail.com>
2025-12-24 15:08:44 +0300

    Remove unnecessary UI related code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -72,53 +72,7 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            showCountryPicker(
-              context: context,
-              //Optional.  Can be used to exclude(remove) one ore more country from the countries list (optional).
-              exclude: <String>['KN', 'MF'],
-              favorite: <String>['SE'],
-              //Optional. Shows phone code before the country name.
-              showPhoneCode: true,
-              onSelect: (Country country) {
-                print('Select country: ${country.displayName}');
-              },
-              // Optional. Sheet moves when keyboard opens.
-              moveAlongWithKeyboard: false,
-              // Optional. Sets the theme for the country list picker.
-              countryListTheme: CountryListThemeData(
-                // Optional. Sets the border radius for the bottomsheet.
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40.0),
-                  topRight: Radius.circular(40.0),
-                ),
-                // Optional. Styles the search field.
-                inputDecoration: InputDecoration(
-                  labelText: 'Search',
-                  hintText: 'Start typing to search',
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: const Color(0xFF8C98A8).withValues(alpha: 0.2),
-                    ),
-                  ),
-                ),
-                // Optional. Styles the text in the search field
-                searchTextStyle: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 18,
-                ),
-              ),
-              header: Padding(
-                padding: const EdgeInsets.only(top: 10.0, bottom: 10.0, left: 20.0, right: 20.0),
-                child: const Text(
-                  'Select your country',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            );
+            ;
           },
           child: const Text('Show country picker'),
         ),
